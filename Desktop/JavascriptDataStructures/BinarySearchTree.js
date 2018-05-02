209953d0166e025c356658ab06e59144431085d0
Arzen Edillo <skrillathegoon@gmail.com>
2018-05-01 23:44:24 -0400

    This is a comment and still trying to figure out how to inline git commit without vim coming up.

---
+++ Desktop/JavascriptDataStructures/BinarySearchTree.js
@@ -0,0 +1,74 @@
+// Factorial (!)
+// 4! = 4 * 3 * 2 * 1 = 24
+// 3! = 3 * 2 * 1 = 6
+/* Recursion example
+function func() {
+  if(//base case){
+    
+  }else {
+    func()
+  }
+}
+ecursion
+function factorial(num){
+  if(num === 1){
+    return num;
+  }else{
+    return num * factorial(num -1);
+  }
+}
+
+factorial(4);
+*/
+// Binary Search Tree
+function BST(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+// Binary Search Tree 
+// Insert function in to the BST binary search tree
+BST.prototype.insert = function(value) {
+  if(value <= this.value){
+    if(!this.left) this.left = new BST(value);
+    else this.left.insert(value);
+  }
+  else if (value > this.value){
+    if(!this.right) this.right = new BST(value)
+    else this.right.insert(value);
+  }
+};
+
+BST.prototype.contains = function(value){
+  if(value === this.value) return true;
+  else if(value < this.value){
+    if (!this.left) return false;
+    else return this.left.contains(value);
+  }
+  else if (value > this.value){
+    if(!this.right) return false;
+    else return this.right.contains(value);
+  }
+};
+
+var bst = new BST(50);
+
+bst.insert(30);
+bst.insert(70);
+bst.insert(30);
+bst.insert(100);
+bst.insert(60);
+bst.insert(59);
+bst.insert(20);
+bst.insert(45);
+bst.insert(35);
+bst.insert(85);
+bst.insert(105);
+bst.insert(10);
+/* Insert method search test 
+console.log(bst.right.right); 100
+*/
+/* Contains method tester
+console.log(bst.contains(85)); true
+console.log(bst.contains(15)); false
+*/
